8db0ed918753755ada9f4efa92c89b2fc8da26e2
Marco Borchi <borchimarco@gmail.com>
2021-11-12 10:25:34 +0100

    clean up the mess

--- webserver/server.py
+++ webserver/server.py
@@ -16,7 +16,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-''' 
+
 class StatusCode(object):
     def __init__(self, code, reason):
         self.code = code
@@ -53,8 +53,6 @@ class Connection():
         delattr(self, '_address')
 
  
-'''
-''' 
 class Response():
     send_buffer_size = 1024
     http_version = 'HTTP/1.0'
@@ -75,7 +73,7 @@ class Response():
           connection.sendall(self.body)
 
           
- '''
+
 
 
 def mini_blink(pin):
@@ -83,7 +81,6 @@ def mini_blink(pin):
     time.sleep(2)
     pin.on()
 
->
 while True:
     conn, addr = s.accept()
     request = conn.recv(1024)
@@ -95,44 +92,3 @@ while True:
     conn.send('Connection: close\n\n')
     conn.sendall('{"status": "ok"}')
     conn.close()
-
-    ''' 
-class StatusCode(object):
-    def __init__(self, code, reason):
-        self.code = code
-        self.reason = reason
-
-
-STATUS_MAP = {
-    '200': StatusCode(200, 'OK')
-}
-
-
-class Connection():
-    def __init__(self, socket):
-        self.socket = socket.accept()
-
-    @property
-    def connection(self):
-        if not hasattr(self, '_connection'):
-            self.get_connection()
-        return self._connection
-
-    @property
-    def address(self):
-        if not hasattr(self, '_address'):
-            self.get_connection()
-        return self._address
-
-    def get_connection(self):
-        self._connection, self._address = self.socket.accept()
-
-    def close(self):
-        self.connection.close()
-        delattr(self, '_connection')
-        delattr(self, '_address')
-
-
-        Content = b'GET / HTTP/1.1\r\nHost: 192.168.1.36\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\nAccept-Language: en-GB,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nSec-GPC: 1\r\n\r\n'
-    
-    '''
